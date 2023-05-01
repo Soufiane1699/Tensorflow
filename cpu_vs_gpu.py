605410c69bbafd65404f03c940eddafc114c15c9
Soufiane1699 <soufianeb14@gmail.com>
2023-05-01 20:31:39 +0200

    Add Comment

--- cpu_vs_gpu.py
+++ cpu_vs_gpu.py
@@ -3,16 +3,21 @@ import time
 
 def matrix_multiplikation(matrix_size, device_name):
     with tf.device(device_name):
+        # erstellt eine Matrix der Größe matrix_size * matrix_size und Datentyp tf.float32
         A = tf.random.normal([matrix_size, matrix_size], dtype=tf.float32)
         B = tf.random.normal([matrix_size, matrix_size], dtype=tf.float32)
+        # Speichert die aktuelle Zeit in der Variablen start_time als Startzeitpunkt
         start_time = time.time()
+        # führt eine Matrixmultiplikation zwischen der Matrix A und B durch
         C = tf.matmul(A, B)
         elapsed_time = time.time() - start_time
     return elapsed_time
 
 def main():
-    matrix_size = 15000
+    matrix_size = 20000
 
+    # "/cpu:0" definiert das Gerät auf dem die jeweilige Berechnung durchgeführt werden soll
+    # TensorFlow Syntax /gerätetyp:geräteindex und das ganze als Zeichenkette
     cpu_time = matrix_multiplikation(matrix_size, "/cpu:0")
     print(f"Matrixmultiplikation auf der CPU abgeschlosse in {cpu_time:.4f} Sekunden.")
 
